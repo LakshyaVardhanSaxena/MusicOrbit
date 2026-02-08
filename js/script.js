39a3a829bc0d85a2c5e460ac82805a1400e10323
Lakshya Vardhan Saxena <lakshyavardhansaxena@gmail.com>
2026-02-08 17:18:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currentFolder = folder;
-    let a = await fetch(`/${folder}/`);
+    let a = await fetch(`http://127.0.0.1:5500/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
